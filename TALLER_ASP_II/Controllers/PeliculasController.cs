1004dcadf8831288e9f28f1db8668ddb4fab6dff
zzamael12114 <zzamael12114@gmail.com>
2020-10-01 20:55:00 -0600

    Intento con base de datos 1

---
+++ TALLER_ASP_II/Controllers/PeliculasController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TALLER_ASP_II.Models;
+using TALLER_ASP_II.Models.VIEWPELICULAS;
+
+namespace TALLER_ASP_II.Controllers
+{
+    public class PeliculasController : Controller
+    {
+        // GET: Peliculas
+        public ActionResult Index() 
+        {
+            List<LISTAPELISVIEWMODEL> lst;
+            using (CinePlusEntities DB=new CinePlusEntities())
+            {
+                 lst = (from d in DB.Peliculas
+                           select new LISTAPELISVIEWMODEL
+                           {
+                               idPelicula = d.idPelicula,
+                               Director = d.Director,
+                               Sinopsis = d.Sinopsis,
+                               Género = d.Género,
+                               TituloPelicula = d.TituloPelicula,
+
+                               /////???????
+                               //Calificacion=d.Calificacion,
+
+
+
+                               //////////////////
+                               ///POSTER???///////
+                               ///
+                           }).ToList();
+            }
+                return View(lst);
+        }
+    }
+}
